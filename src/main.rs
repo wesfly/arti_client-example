dda6f2f21e6aff1557c0d08b53a2f9dbf15584c9
wesfly <wesfly@noreply.codeberg.org>
2025-08-16 22:35:32 +0200

    Remove debug_assertions

--- src/main.rs
+++ src/main.rs
@@ -24,24 +24,20 @@ pub fn get_tor_client() -> Result<TorClient<PreferredRuntime>> {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    #[cfg(debug_assertions)]
     eprintln!("getting shared Tor client...");
 
     let tor_client = get_tor_client()?;
     // The BBC tor site
     let onion_address = "www.bbcweb3hytmzhn5d532owbu6oqadra5z3ar726vq5kgwwn6aucdccrad.onion:80";
 
-    #[cfg(debug_assertions)]
     eprintln!("connecting to hidden service: {onion_address}");
     let mut stream = tor_client.connect(onion_address).await?;
 
-    #[cfg(debug_assertions)]
     eprintln!("sending request...");
     let request = format!("GET / HTTP/1.1\r\nHost: {onion_address}\r\nConnection: close\r\n\r\n)");
     stream.write_all(request.as_bytes()).await?;
     stream.flush().await?;
 
-    #[cfg(debug_assertions)]
     eprintln!("reading response...");
     let mut buf = Vec::new();
     stream.read_to_end(&mut buf).await?;
